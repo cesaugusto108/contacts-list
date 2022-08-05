8ce7a114cdad9953f5a1f0c883ccef2a5aa6e9cd
Cesar Augusto <ces.augusto108@gmail.com>
2022-08-05 00:15:41 -0300

    Delete unused SQL script version

--- SQL_script/scipt_bd.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE ContactsList;
-USE ContactsList;
-SHOW TABLES;
-CREATE TABLE Contacts (
-	ID int primary key auto_increment,
-    	CONTACT_NAME varchar(80) not null,
-    	EMAIL varchar(80),
-    	TELEFONE bigint not null
-);
